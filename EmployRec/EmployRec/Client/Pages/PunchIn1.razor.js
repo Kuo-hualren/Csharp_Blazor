65905ce7a827eac0f9aa6dc6c7c25dc4de587875
Kuo-hualren <Kuo-hualren@gmail.com>
2023-07-22 05:03:55 +0800

    second commit

---
+++ EmployRec/EmployRec/Client/Pages/PunchIn1.razor.js
@@ -0,0 +1,5 @@
+﻿export function GetValue() {
+    const inputField = document.getElementById('result').textContent;
+    console.log(inputField);
+    return inputField;
+}
---
+++ EmployRec/EmployRec/Client/wwwroot/ScannerScript/scannerFunction.js
@@ -0,0 +1,26 @@
+﻿window.loadQrScript = function () {
+    const script = document.createElement('script');
+    script.src = './ScannerScript/qrScript.js';
+    document.body.appendChild(script);
+};
+
+window.startQrScanner = function () {
+    const html5Qrcode = new Html5Qrcode('reader');
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+        if (decodedText) {
+            document.getElementById('show').style.display = 'block';
+            document.getElementById('result').textContent = decodedText;
+            document.getElementById('result').style.display = 'none';
+            html5Qrcode.stop();
+        }
+    };
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    html5Qrcode.start({ facingMode: 'environment' }, config, qrCodeSuccessCallback);
+};
+
+window.GetValue = function () {
+    const inputField = document.getElementById('result').textContent;
+    console.log(inputField);
+    return inputField.value;
+}
+
